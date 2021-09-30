43628d278b67566ec1fdfa759c833d61ec6cd0d5
dev-to2k <trungnek175@gmail.com>
2021-09-30 15:50:47 +0700

    #Edit: add header, footer component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,18 +2,22 @@ import React from 'react';
 import './styles/App.css';
 import {BrowserRouter, Route, Switch} from "react-router-dom";
 import PageRender from "./PageRender";
+import Header from "./components/global/Header";
+import Footer from "./components/global/Footer";
 
 function App() {
   return (
-    <div className="">
-      <BrowserRouter>
+    <BrowserRouter>
+      <Header/>
+      <div className="container px-5 mx-auto">
         <Switch>
-          <Route exact path="/" component={PageRender} />
-          <Route exact path="/:page" component={PageRender} />
-          <Route exact path="/:page/:slug" component={PageRender} />
+          <Route exact path="/" component={PageRender}/>
+          <Route exact path="/:page" component={PageRender}/>
+          <Route exact path="/:page/:slug" component={PageRender}/>
         </Switch>
-      </BrowserRouter>
-    </div>
+      </div>
+      <Footer/>
+    </BrowserRouter>
   );
 }
 
