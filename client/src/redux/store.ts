fb67960aba4eac706376cf055ba6fa1d7432ec8d
dev-to2k <trungnek175@gmail.com>
2021-09-30 15:57:09 +0700

    #Redux: create store

---
+++ client/src/redux/store.ts
@@ -0,0 +1,8 @@
+import rootReducer from "./reducers"
+import {applyMiddleware, createStore} from "redux"
+import thunk from 'redux-thunk'
+import {composeWithDevTools} from 'redux-devtools-extension'
+
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)))
+
+export default store
