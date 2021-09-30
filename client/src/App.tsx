6e5e5288b1425e4e5f8e9e0a73fffb4c3c665338
dev-to2k <trungnek175@gmail.com>
2021-09-30 12:57:31 +0700

    #Route: route follow PageRender component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './styles/App.css';
+import {BrowserRouter, Route, Switch} from "react-router-dom";
+import PageRender from "./PageRender";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={PageRender} />
+          <Route exact path="/:page" component={PageRender} />
+          <Route exact path="/:page/:slug" component={PageRender} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
