c1c5a7975311e5841b8994d8725bbc536b4c3876
dev-to2k <trungnek175@gmail.com>
2021-10-02 14:26:31 +0700

    #Redux: create alert reducer

---
+++ client/src/redux/reducers/alertReducer.ts
@@ -0,0 +1,13 @@
+import {IAlert} from "../../utils/TypeScript";
+import {ALERT, IAlertType} from "../types/alertTye";
+
+const alertReducer = (state: IAlert = {}, action: IAlertType): IAlert => {
+  switch (action.type) {
+    case ALERT:
+      return action.payload
+    default:
+      return state
+  }
+}
+
+export default alertReducer
