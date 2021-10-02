a3f4165b493c18b49825495bcccc182f040c6bbc
dev-to2k <trungnek175@gmail.com>
2021-10-02 14:29:19 +0700

    #Component: create loading component

---
+++ client/src/components/alert/Loading.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Loading = () => {
+  return (
+    <div className="fixed w-full h-full z-10 bg-white flex flex-col justify-center items-center">
+      <div className="animate-spin mb-5 rounded-full h-32 w-32 border-t-2 border-b-2 border-purple-500"></div>
+      <p>Loading...</p>
+    </div>
+  );
+};
+
+export default Loading;
