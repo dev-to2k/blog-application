e9ec58363b2609ddf65eacc388a9f72758f4cce5
dev-to2k <trungnek175@gmail.com>
2021-10-02 14:28:13 +0700

    #Edit: edit name logo

--- client/src/components/global/Header.tsx
+++ client/src/components/global/Header.tsx
@@ -17,7 +17,7 @@ const Header = () => {
             <path
               d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
           </svg>
-          <span className="ml-3 text-xl">Tailblocks</span>
+          <span className="ml-3 text-xl">BlogTo2k</span>
         </a>
         <Search />
         <Menu/>
