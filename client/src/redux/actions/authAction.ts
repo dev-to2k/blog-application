a0a7418be9b0224fc335480b91a5a8a300b2159d
dev-to2k <trungnek175@gmail.com>
2021-10-04 19:31:44 +0700

    create refresh token and log out actions

--- client/src/redux/actions/authAction.ts
+++ client/src/redux/actions/authAction.ts
@@ -1,56 +1,87 @@
-import { IUserLogin, IUserRegister } from "../../utils/TypeScript";
-import { AUTH, IAuthType } from "../types/authType";
-import { Dispatch } from "redux";
-import { postAPI } from "../../utils/FetchData";
-import { ALERT, IAlertType } from "../types/alertTye";
-import { validRegister } from "../../utils/Valid";
+import {IUserLogin, IUserRegister} from "../../utils/TypeScript";
+import {AUTH, IAuthType} from "../types/authType";
+import {Dispatch} from "redux";
+import {getAPI, postAPI} from "../../utils/FetchData";
+import {ALERT, IAlertType} from "../types/alertTye";
+import {validRegister} from "../../utils/Valid";
 
 export const login =
   (userLogin: IUserLogin) =>
-  async (dispatch: Dispatch<IAuthType | IAlertType>) => {
-    try {
-      dispatch({
-        type: ALERT,
-        payload: {
-          loading: true,
-        },
-      });
+    async (dispatch: Dispatch<IAuthType | IAlertType>) => {
+      try {
+        dispatch({
+          type: ALERT,
+          payload: {
+            loading: true,
+          },
+        });
 
-      const res = await postAPI("login", userLogin);
+        const res = await postAPI("login", userLogin);
 
-      dispatch({ type: AUTH, payload: res.data });
+        dispatch({type: AUTH, payload: res.data});
 
-      dispatch({
-        type: ALERT,
-        payload: {
-          success: "Login success!",
-        },
-      });
-    } catch (e: any) {
-      dispatch({
-        type: ALERT,
-        payload: {
-          errors: e.response.data.msg,
-        },
-      });
-    }
-  };
+        dispatch({
+          type: ALERT,
+          payload: {
+            success: "Login success!",
+          },
+        });
+
+        localStorage.setItem('logged', 'to2k-channel')
+      } catch (e: any) {
+        dispatch({
+          type: ALERT,
+          payload: {
+            errors: e.response.data.msg,
+          },
+        });
+      }
+    };
 
 export const register =
   (userRegister: IUserRegister) =>
-  async (dispatch: Dispatch<IAuthType | IAlertType>) => {
-    const check = validRegister(userRegister);
+    async (dispatch: Dispatch<IAuthType | IAlertType>) => {
+      const check = validRegister(userRegister);
 
-    if (check.errLength > 0)
-      return dispatch({ type: ALERT, payload: { errors: check.errMsg } });
+      if (check.errLength > 0)
+        return dispatch({type: ALERT, payload: {errors: check.errMsg}});
+
+      try {
+        dispatch({type: ALERT, payload: {loading: true}});
+
+        const res = await postAPI("register", userRegister);
+
+        dispatch({type: ALERT, payload: {success: res.data.msg}});
+      } catch (err: any) {
+        dispatch({type: ALERT, payload: {errors: err.response.data.msg}});
+      }
+    };
+
+export const refreshToken =
+  () => async (dispatch: Dispatch<IAuthType | IAlertType>) => {
+    const logged = localStorage.getItem("logged");
+    if (logged !== "to2k-channel") return;
 
     try {
-      dispatch({ type: ALERT, payload: { loading: true } });
+      dispatch({type: ALERT, payload: {loading: true}});
 
-      const res = await postAPI("register", userRegister);
+      const res = await getAPI("refresh_token");
 
-      dispatch({ type: ALERT, payload: { success: res.data.msg } });
+      dispatch({type: AUTH, payload: res.data});
+
+      dispatch({type: ALERT, payload: {}});
     } catch (err: any) {
-      dispatch({ type: ALERT, payload: { errors: err.response.data.msg } });
+      dispatch({type: ALERT, payload: {errors: err.response.data.msg}});
+    }
+  };
+
+export const logout =
+  () => async (dispatch: Dispatch<IAuthType | IAlertType>) => {
+    try {
+      localStorage.removeItem("logged");
+      await getAPI("logout");
+      window.location.href = "/";
+    } catch (err: any) {
+      dispatch({type: ALERT, payload: {errors: err.response.data.msg}});
     }
   };
