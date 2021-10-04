2e3c61a2d43fb3c22be7abade4defbd3af36ca4b
dev-to2k <trungnek175@gmail.com>
2021-10-04 10:46:22 +0700

    create register page

--- client/src/pages/register.tsx
+++ client/src/pages/register.tsx
@@ -1,10 +1,31 @@
 import React from 'react';
+import RegisterForm from "../components/auth/RegisterForm";
+import {Link} from "react-router-dom";
 
 const Register = () => {
   return (
-    <div>
-      <h2>Register Page</h2>
-    </div>
+    <>
+      <div className="min-h-screen rounded-lg flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
+        <div className="max-w-md w-full space-y-8">
+          <div>
+            {/*<img className="mx-auto h-12 w-auto" src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg" alt="Workflow" />*/}
+            <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+              Sign up to your account
+            </h2>
+          </div>
+          <div className="rounded-lg bg-white max-w-md rounded overflow-hidden shadow-xl p-5">
+            <RegisterForm/>
+            <div className="text-sm text-center mt-3">
+              <p>Have already an account?&nbsp;
+                <Link to={`/login`} className="font-medium text-indigo-600 hover:text-indigo-500">
+                  Login
+                </Link>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
