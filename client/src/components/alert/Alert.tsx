f440cea7ed95920626edcac75d0f76c1a9bd231c
dev-to2k <trungnek175@gmail.com>
2021-10-04 19:29:22 +0700

    edit icon

--- client/src/components/alert/Alert.tsx
+++ client/src/components/alert/Alert.tsx
@@ -1,9 +1,9 @@
 import React from "react";
-import { RootStore } from "../../utils/TypeScript";
+import {RootStore} from "../../utils/TypeScript";
 
 import Loading from "./Loading";
 import Toast from "./Toast";
-import { useSelector } from "react-redux";
+import {useSelector} from "react-redux";
 import {
   CheckCircleIcon,
   CheckIcon,
@@ -11,16 +11,16 @@ import {
 } from "@heroicons/react/solid";
 
 export const Alert = () => {
-  const { alertReducer } = useSelector((state: RootStore) => state);
+  const {alertReducer} = useSelector((state: RootStore) => state);
   return (
     <div>
-      {alertReducer.loading && <Loading />}
+      {alertReducer.loading && <Loading/>}
 
       {alertReducer.errors && (
         <Toast
           title="Error"
           body={alertReducer.errors}
-          icon={<XCircleIcon className={`w-7 text-white`} />}
+          icon={<XCircleIcon className={`w-7 text-white`}/>}
           textColor="text-red-500"
           bgColor="bg-red-500"
         />
@@ -30,7 +30,7 @@ export const Alert = () => {
         <Toast
           title="Success"
           body={alertReducer.success}
-          icon={<CheckCircleIcon className={`w-7 text-white`} />}
+          icon={<CheckCircleIcon className={`w-7 text-white`}/>}
           textColor="text-green-500"
           bgColor="bg-green-500"
         />
@@ -43,11 +43,11 @@ export const showErrMsg = (msg: string) => {
   return (
     <div
       className="shadow-lg text-center mx-auto my-5 rounded-lg"
-      style={{ width: "350px" }}
+      style={{width: "350px"}}
     >
       <div className="p-5">
-        <h5 className="text-xl font-semibold mb-2">
-          <CheckCircleIcon className={`w-14 mx-auto text-green-600`} />
+        <h5 className="text-xl text-center font-semibold mb-2">
+          <XCircleIcon className={`w-14 mx-auto text-red-600`}/>
         </h5>
         <p className="mb-4">{msg}</p>
       </div>
@@ -59,11 +59,11 @@ export const showSuccessMsg = (msg: string) => {
   return (
     <div
       className="shadow-lg mx-auto my-5 rounded-lg"
-      style={{ width: "350px" }}
+      style={{width: "350px"}}
     >
       <div className="p-5">
-        <h5 className="text-xl font-semibold mb-2">
-          <XCircleIcon className={`w-14 text-red-600`} />
+        <h5 className="text-xl text-center font-semibold mb-2">
+          <CheckCircleIcon className={`w-14 text-green-600`}/>
         </h5>
         <p className="mb-4">{msg}</p>
       </div>
