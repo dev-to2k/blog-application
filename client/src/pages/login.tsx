25c081eb1632fbd3d650449f18931111bebb2c22
dev-to2k <trungnek175@gmail.com>
2021-10-04 10:45:57 +0700

    nothing

--- client/src/pages/login.tsx
+++ client/src/pages/login.tsx
@@ -7,7 +7,6 @@ const Login = () => {
   const [sms, setSms] = useState(false)
   return (
     <>
-
       <div className="min-h-screen rounded-lg flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
         <div className="max-w-md w-full space-y-8">
           <div>
