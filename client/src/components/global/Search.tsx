b5fb285321adea016cca023a109228036dfd15fe
dev-to2k <trungnek175@gmail.com>
2021-10-04 19:43:30 +0700

    format

--- client/src/components/global/Search.tsx
+++ client/src/components/global/Search.tsx
@@ -1,9 +1,9 @@
 import React from "react";
-import { SearchIcon } from "@heroicons/react/solid";
+import {SearchIcon} from "@heroicons/react/solid";
 
 const Search = () => {
   return (
-    <div className="w-6/12 relative md:ml-auto mb-3 lg:mb-0 text-gray-600">
+    <div className="lg:w-6/12 relative mx-auto md:ml-auto mb-3 md:mb-0 text-gray-600">
       <input
         className="w-full border-1 border-gray-300 bg-white h-10 px-5 pr-16 rounded-lg text-sm focus:outline-none"
         type="search"
@@ -11,7 +11,7 @@ const Search = () => {
         placeholder="Search"
       />
       <button type="submit" className="absolute right-0 top-0 mt-2.5 mr-4">
-        <SearchIcon className="h-5 w-5 text-blue-600" />
+        <SearchIcon className="h-5 w-5 text-blue-600"/>
       </button>
     </div>
   );
