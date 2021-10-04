a31448e2a9e057a97bc6b3d7335720e23f23ce93
dev-to2k <trungnek175@gmail.com>
2021-10-04 11:31:55 +0700

    format

--- client/src/redux/reducers/index.ts
+++ client/src/redux/reducers/index.ts
@@ -1,8 +1,8 @@
-import {combineReducers} from "redux";
+import { combineReducers } from "redux";
 import authReducer from "./authReducer";
 import alertReducer from "./alertReducer";
 
 export default combineReducers({
   authReducer,
-  alertReducer
-})
+  alertReducer,
+});
