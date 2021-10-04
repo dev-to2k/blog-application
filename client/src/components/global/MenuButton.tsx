4a5c5a8865d78914d5ac8b63e29802ea6f506716
dev-to2k <trungnek175@gmail.com>
2021-10-04 22:50:23 +0700

    format

--- client/src/components/global/MenuButton.tsx
+++ client/src/components/global/MenuButton.tsx
@@ -1,8 +1,8 @@
 /* This example requires Tailwind CSS v2.0+ */
-import React, { Fragment } from "react";
-import { Menu, Transition } from "@headlessui/react";
-import { ChevronDownIcon } from "@heroicons/react/solid";
-import { logout } from "../../redux/actions/authAction";
+import React, {Fragment} from "react";
+import {Menu, Transition} from "@headlessui/react";
+import {ChevronDownIcon, LogoutIcon} from "@heroicons/react/outline";
+import {logout} from "../../redux/actions/authAction";
 
 function classNames(...classes: string[]) {
   return classes.filter(Boolean).join(" ");
@@ -14,13 +14,14 @@ interface IProps {
 }
 
 export default function MenuButton(props: IProps) {
-  const { auth, dispatch } = props;
+  const {auth, dispatch} = props;
   return (
     <Menu as="div" className="relative inline-block text-left">
       <div>
-        <Menu.Button className="inline-flex justify-center w-full rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500">
+        <Menu.Button
+          className="inline-flex justify-center w-full rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500">
           {auth.user.name}
-          <ChevronDownIcon className="-mr-1 ml-2 h-5 w-5" aria-hidden="true" />
+          <ChevronDownIcon className="-mr-1 ml-2 h-5 w-5" aria-hidden="true"/>
         </Menu.Button>
       </div>
 
@@ -33,10 +34,11 @@ export default function MenuButton(props: IProps) {
         leaveFrom="transform opacity-100 scale-100"
         leaveTo="transform opacity-0 scale-95"
       >
-        <Menu.Items className="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
+        <Menu.Items
+          className="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
           <div className="py-1">
             <Menu.Item>
-              {({ active }) => (
+              {({active}) => (
                 <a
                   href="#"
                   className={classNames(
@@ -44,26 +46,29 @@ export default function MenuButton(props: IProps) {
                     "block px-4 py-2 text-sm"
                   )}
                 >
-                  <img
-                    className="inline object-cover w-8 h-8 rounded-full mr-2"
-                    src={auth.user.avatar}
-                    alt="Profile image"
-                  />
-                  Profile
+                  <div className="flex items-center">
+                    <img
+                      className="inline object-cover w-6 h-6 rounded-full mr-2"
+                      src={auth.user.avatar}
+                      alt="Profile image"
+                    />
+                    <div className="text-xs">{auth.user.account}</div>
+                  </div>
                 </a>
               )}
             </Menu.Item>
             <form method="POST" action="#">
               <Menu.Item>
-                {({ active }) => (
+                {({active}) => (
                   <button
                     type="submit"
                     onClick={() => dispatch(logout())}
                     className={classNames(
                       active ? "bg-gray-100 text-gray-900" : "text-gray-700",
-                      "block w-full text-left px-4 py-2 text-sm"
+                      "block w-full text-left px-4 py-2 text-sm flex items-center"
                     )}
                   >
+                    <LogoutIcon className={`w-5 h-5 text-gray-400 mr-2`}/>
                     Sign out
                   </button>
                 )}
