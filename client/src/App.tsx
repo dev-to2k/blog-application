b3c30b0757fc2e187de7cf84bebc632eb32ea040
dev-to2k <trungnek175@gmail.com>
2021-10-04 11:26:58 +0700

    format

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,24 @@
-import React from 'react';
-import './styles/App.css';
-import {BrowserRouter, Route, Switch} from "react-router-dom";
+import React from "react";
+import "./styles/App.css";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import PageRender from "./PageRender";
 import Header from "./components/global/Header";
 import Footer from "./components/global/Footer";
-import Alert from "./components/alert/Alert";
+import { Alert } from "./components/alert/Alert";
 
 function App() {
   return (
     <BrowserRouter>
-      <Alert/>
-      <Header/>
+      <Alert />
+      <Header />
       <div className="container px-5 mx-auto">
         <Switch>
-          <Route exact path="/" component={PageRender}/>
-          <Route exact path="/:page" component={PageRender}/>
-          <Route exact path="/:page/:slug" component={PageRender}/>
+          <Route exact path="/" component={PageRender} />
+          <Route exact path="/:page" component={PageRender} />
+          <Route exact path="/:page/:slug" component={PageRender} />
         </Switch>
       </div>
-      <Footer/>
+      <Footer />
     </BrowserRouter>
   );
 }
