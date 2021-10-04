a6e78faf21e018cb398cd16d1189b2d8c02e0dce
dev-to2k <trungnek175@gmail.com>
2021-10-04 10:45:26 +0700

    create register action

--- client/src/redux/actions/authAction.ts
+++ client/src/redux/actions/authAction.ts
@@ -1,8 +1,9 @@
-import {IUserLogin} from "../../utils/TypeScript";
+import {IUserLogin, IUserRegister} from "../../utils/TypeScript";
 import {AUTH, IAuthType} from "../types/authType";
 import {Dispatch} from "redux";
 import {postAPI} from "../../utils/FetchData";
 import {ALERT, IAlertType} from "../types/alertTye";
+import {validRegister} from "../../utils/Valid";
 
 export const login = (userLogin: IUserLogin) => async (dispatch: Dispatch<IAuthType | IAlertType>) => {
   try {
@@ -38,4 +39,21 @@ export const login = (userLogin: IUserLogin) => async (dispatch: Dispatch<IAuthT
     })
   }
 
+}
+
+export const register = (userRegister: IUserRegister) => async (dispatch: Dispatch<IAuthType | IAlertType>) => {
+  const check = validRegister(userRegister)
+
+  if (check.errLength > 0)
+    return dispatch({type: ALERT, payload: {errors: check.errMsg}})
+
+  try {
+    dispatch({type: ALERT, payload: {loading: true}})
+
+    const res = await postAPI('register', userRegister)
+
+    dispatch({type: ALERT, payload: {success: res.data.msg}})
+  } catch (err: any) {
+    dispatch({type: ALERT, payload: {errors: err.response.data.msg}})
+  }
 }
