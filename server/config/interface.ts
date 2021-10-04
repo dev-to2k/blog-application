57e437d8b7b6b19739c20c7e2c3ef6d5529c6cd8
dev-to2k <trungnek175@gmail.com>
2021-10-04 22:49:37 +0700

    create interface for login with Google

--- server/config/interface.ts
+++ server/config/interface.ts
@@ -22,3 +22,18 @@ export interface IDecodedToken {
   iat: number
   exp: number
 }
+
+export interface IGgPayload {
+  email: string
+  email_verified: boolean
+  name: string
+  picture: string
+}
+
+export interface IUserParams {
+  name: string
+  account: string
+  password: string
+  avatar?: string
+  type: string
+}
