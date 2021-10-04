b3ae6465a89d72c59e44a653915dc297dcc5a93c
dev-to2k <trungnek175@gmail.com>
2021-10-04 11:27:58 +0700

    edit dispatch type auth in login

--- client/src/redux/actions/authAction.ts
+++ client/src/redux/actions/authAction.ts
@@ -1,59 +1,56 @@
-import {IUserLogin, IUserRegister} from "../../utils/TypeScript";
-import {AUTH, IAuthType} from "../types/authType";
-import {Dispatch} from "redux";
-import {postAPI} from "../../utils/FetchData";
-import {ALERT, IAlertType} from "../types/alertTye";
-import {validRegister} from "../../utils/Valid";
+import { IUserLogin, IUserRegister } from "../../utils/TypeScript";
+import { AUTH, IAuthType } from "../types/authType";
+import { Dispatch } from "redux";
+import { postAPI } from "../../utils/FetchData";
+import { ALERT, IAlertType } from "../types/alertTye";
+import { validRegister } from "../../utils/Valid";
 
-export const login = (userLogin: IUserLogin) => async (dispatch: Dispatch<IAuthType | IAlertType>) => {
-  try {
-    dispatch({
-      type: ALERT,
-      payload: {
-        loading: true
-      }
-    })
+export const login =
+  (userLogin: IUserLogin) =>
+  async (dispatch: Dispatch<IAuthType | IAlertType>) => {
+    try {
+      dispatch({
+        type: ALERT,
+        payload: {
+          loading: true,
+        },
+      });
 
-    const res = await postAPI('login', userLogin)
+      const res = await postAPI("login", userLogin);
 
-    dispatch({
-      type: AUTH,
-      payload: {
-        token: res.data.access_token,
-        user: res.data.user
-      }
-    })
+      dispatch({ type: AUTH, payload: res.data });
 
-    dispatch({
-      type: ALERT,
-      payload: {
-        success: 'Login success!'
-      }
-    })
-  } catch (e: any) {
-    dispatch({
-      type: ALERT,
-      payload: {
-        errors: e.response.data.msg
-      }
-    })
-  }
+      dispatch({
+        type: ALERT,
+        payload: {
+          success: "Login success!",
+        },
+      });
+    } catch (e: any) {
+      dispatch({
+        type: ALERT,
+        payload: {
+          errors: e.response.data.msg,
+        },
+      });
+    }
+  };
 
-}
+export const register =
+  (userRegister: IUserRegister) =>
+  async (dispatch: Dispatch<IAuthType | IAlertType>) => {
+    const check = validRegister(userRegister);
 
-export const register = (userRegister: IUserRegister) => async (dispatch: Dispatch<IAuthType | IAlertType>) => {
-  const check = validRegister(userRegister)
+    if (check.errLength > 0)
+      return dispatch({ type: ALERT, payload: { errors: check.errMsg } });
 
-  if (check.errLength > 0)
-    return dispatch({type: ALERT, payload: {errors: check.errMsg}})
+    try {
+      dispatch({ type: ALERT, payload: { loading: true } });
 
-  try {
-    dispatch({type: ALERT, payload: {loading: true}})
+      const res = await postAPI("register", userRegister);
 
-    const res = await postAPI('register', userRegister)
-
-    dispatch({type: ALERT, payload: {success: res.data.msg}})
-  } catch (err: any) {
-    dispatch({type: ALERT, payload: {errors: err.response.data.msg}})
-  }
-}
+      dispatch({ type: ALERT, payload: { success: res.data.msg } });
+    } catch (err: any) {
+      dispatch({ type: ALERT, payload: { errors: err.response.data.msg } });
+    }
+  };
