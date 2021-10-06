b1ce32cc0d531522d01d4e86b2692eedb78f5ed7
dev-to2k <trungnek175@gmail.com>
2021-10-06 21:20:05 +0700

    create middleware auth user

---
+++ server/middleware/auth.ts
@@ -0,0 +1,28 @@
+import { IDecodedToken, IReqAuth } from "../config/interface";
+import { NextFunction, Response } from "express";
+import jwt from "jsonwebtoken";
+import Users from "../models/userModel";
+
+const auth = async (req: IReqAuth, res: Response, next: NextFunction) => {
+  try {
+    const token = req.header("Authorization");
+    if (!token) return res.status(400).json({ msg: "Invalid Authentication." });
+
+    const decoded = <IDecodedToken>(
+      jwt.verify(token, `${process.env.ACCESS_TOKEN_SECRET}`)
+    );
+    if (!decoded)
+      return res.status(400).json({ msg: "Invalid Authentication." });
+
+    const user = await Users.findOne({ _id: decoded.id });
+    if (!user) return res.status(400).json({ msg: "User does not exist." });
+
+    req.user = user;
+
+    next();
+  } catch (err: any) {
+    return res.status(500).json({ msg: err.message });
+  }
+};
+
+export default auth;
