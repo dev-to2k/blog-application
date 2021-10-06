f4fe1a059b6bdfff1c3eea720d02ced73409c9f7
dev-to2k <trungnek175@gmail.com>
2021-10-06 15:52:04 +0700

    send OTP SMS

--- server/config/sendSMS.ts
+++ server/config/sendSMS.ts
@@ -1,8 +1,9 @@
-import { Twilio } from 'twilio'
+import {Twilio} from 'twilio'
 
 const accountSid = `${process.env.TWILIO_ACCOUNT_SID}`;
 const authToken = `${process.env.TWILIO_AUTH_TOKEN}`;
 const from = `${process.env.TWILIO_PHONE_NUMBER}`;
+const serviceID = `${process.env.TWILIO_SERVICE_ID}`;
 
 const client = new Twilio(accountSid, authToken);
 
@@ -20,3 +21,36 @@ export const sendSms = (to: string, body: string, txt: string) => {
   }
 }
 
+export const smsOTP = async (to: string, channel: string) => {
+  try {
+    const data = await client
+      .verify
+      .services(serviceID)
+      .verifications
+      .create({
+        to,
+        channel
+      })
+
+    return data;
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+export const smsVerify = async (to: string, code: string) => {
+  try {
+    const data = await client
+      .verify
+      .services(serviceID)
+      .verificationChecks
+      .create({
+        to,
+        code
+      })
+
+    return data;
+  } catch (err) {
+    console.log(err)
+  }
+}
