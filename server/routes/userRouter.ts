ade4357b5e9c12f86a8d0c9dd169e2fa4cd4ffd3
dev-to2k <trungnek175@gmail.com>
2021-10-06 21:22:54 +0700

    #Feature: update user - router

---
+++ server/routes/userRouter.ts
@@ -0,0 +1,9 @@
+import express from "express";
+import auth from "../middleware/auth";
+import userCtrl from "../controllers/userCtrl";
+
+const router = express.Router();
+
+router.patch("/user", auth, userCtrl.updateUser);
+
+export default router;
