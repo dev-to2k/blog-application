d3c3a0c6c2af6c2db54780c9b45761578b150c80
dev-to2k <trungnek175@gmail.com>
2021-10-06 21:22:37 +0700

    #Feature: update user - server

---
+++ server/controllers/userCtrl.ts
@@ -0,0 +1,28 @@
+import { IReqAuth } from "../config/interface";
+import Users from "../models/userModel";
+import { Response } from "express";
+
+const userCtrl = {
+  updateUser: async (req: IReqAuth, res: Response) => {
+    if (!req.user)
+      return res.status(400).json({ msg: "Invalid Authentication." });
+
+    try {
+      const { avatar, name } = req.body;
+
+      await Users.findOneAndUpdate(
+        { _id: req.user._id },
+        {
+          avatar,
+          name,
+        }
+      );
+
+      res.json({ msg: "Update Success!" });
+    } catch (err: any) {
+      return res.status(500).json({ msg: err.message });
+    }
+  },
+};
+
+export default userCtrl;
