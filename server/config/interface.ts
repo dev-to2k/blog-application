dc8e502a9be649ce1bb1112c3c109314bb7d4d07
dev-to2k <trungnek175@gmail.com>
2021-10-06 21:21:25 +0700

    create request auth interface

--- server/config/interface.ts
+++ server/config/interface.ts
@@ -1,39 +1,44 @@
-import {Document} from 'mongoose'
+import { Document } from "mongoose";
+import { Request } from "express";
 
 export interface IUser extends Document {
-  name: string
-  account: string
-  password: string
-  avatar: string
-  role: string
-  type: string
-  _doc: object
+  name: string;
+  account: string;
+  password: string;
+  avatar: string;
+  role: string;
+  type: string;
+  _doc: object;
 }
 
 export interface INewUser {
-  name: string
-  password: string
-  account: string
+  name: string;
+  password: string;
+  account: string;
 }
 
 export interface IDecodedToken {
-  id?: string
-  newUser?: INewUser
-  iat: number
-  exp: number
+  id?: string;
+  newUser?: INewUser;
+  iat: number;
+  exp: number;
 }
 
 export interface IGgPayload {
-  email: string
-  email_verified: boolean
-  name: string
-  picture: string
+  email: string;
+  email_verified: boolean;
+  name: string;
+  picture: string;
 }
 
 export interface IUserParams {
-  name: string
-  account: string
-  password: string
-  avatar?: string
-  type: string
+  name: string;
+  account: string;
+  password: string;
+  avatar?: string;
+  type: string;
+}
+
+export interface IReqAuth extends Request {
+  user?: IUser;
 }
