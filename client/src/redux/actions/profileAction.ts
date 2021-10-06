7d0dc7569c6788bf414daef47f0460af1e4026b4
dev-to2k <trungnek175@gmail.com>
2021-10-06 21:22:06 +0700

    dispatch token and user object to store

--- client/src/redux/actions/profileAction.ts
+++ client/src/redux/actions/profileAction.ts
@@ -1,11 +1,12 @@
-import { IAuth } from "../types/authType";
+import { AUTH, IAuth, IAuthType } from "../types/authType";
 import { Dispatch } from "redux";
 import { ALERT, IAlertType } from "../types/alertTye";
 import { checkImage, imageUpload } from "../../utils/ImageUpload";
+import { patchAPI } from "../../utils/FetchData";
 
 export const updateUser =
   (avatar: File, name: string, auth: IAuth) =>
-  async (dispatch: Dispatch<IAlertType>) => {
+  async (dispatch: Dispatch<IAlertType | IAuthType>) => {
     if (!auth.access_token || !auth.user) return;
 
     let url = "";
@@ -16,10 +17,31 @@ export const updateUser =
         if (check) return dispatch({ type: ALERT, payload: { errors: check } });
 
         const photo = await imageUpload(avatar);
-        console.log(photo);
+        url = photo.url;
       }
 
-      dispatch({ type: ALERT, payload: { loading: false } });
+      dispatch({
+        type: AUTH,
+        payload: {
+          access_token: auth.access_token,
+          user: {
+            ...auth.user,
+            avatar: url ? url : auth.user.avatar,
+            name: name ? name : auth.user.name,
+          },
+        },
+      });
+
+      const res = await patchAPI(
+        "user",
+        {
+          avatar: url ? url : auth.user.avatar,
+          name: name ? name : auth.user.name,
+        },
+        auth.access_token
+      );
+
+      dispatch({ type: ALERT, payload: { success: res.data.msg } });
     } catch (err: any) {
       dispatch({ type: ALERT, payload: { errors: err.response.data.msg } });
     }
