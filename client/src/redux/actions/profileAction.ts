9a1e79e0ad86d948f3206173223890e8b40fa34c
dev-to2k <trungnek175@gmail.com>
2021-10-06 23:26:03 +0700

    dispatch reset password to store

--- client/src/redux/actions/profileAction.ts
+++ client/src/redux/actions/profileAction.ts
@@ -3,6 +3,7 @@ import { Dispatch } from "redux";
 import { ALERT, IAlertType } from "../types/alertTye";
 import { checkImage, imageUpload } from "../../utils/ImageUpload";
 import { patchAPI } from "../../utils/FetchData";
+import { checkPassword } from "../../utils/Valid";
 
 export const updateUser =
   (avatar: File, name: string, auth: IAuth) =>
@@ -40,6 +41,23 @@ export const updateUser =
         },
         auth.access_token
       );
+
+      dispatch({ type: ALERT, payload: { success: res.data.msg } });
+    } catch (err: any) {
+      dispatch({ type: ALERT, payload: { errors: err.response.data.msg } });
+    }
+  };
+
+export const resetPassword =
+  (password: string, cf_password: string, token: string) =>
+  async (dispatch: Dispatch<IAlertType | IAuthType>) => {
+    const msg = checkPassword(password, cf_password);
+    if (msg) return dispatch({ type: ALERT, payload: { errors: msg } });
+
+    try {
+      dispatch({ type: ALERT, payload: { loading: true } });
+
+      const res = await patchAPI("reset_password", { password }, token);
 
       dispatch({ type: ALERT, payload: { success: res.data.msg } });
     } catch (err: any) {
