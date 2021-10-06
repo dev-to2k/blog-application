1bf67d56f72bf81f8960ceca0ef8fdec2d0b6526
dev-to2k <trungnek175@gmail.com>
2021-10-06 21:20:59 +0700

    use user router

--- server/index.ts
+++ server/index.ts
@@ -1,28 +1,29 @@
-import dotenv from 'dotenv'
-dotenv.config()
+import dotenv from "dotenv";
+dotenv.config();
 
-import express from 'express'
-import cors from 'cors'
-import cookieParser from 'cookie-parser'
-import morgan from 'morgan'
-import routes from './routes/index'
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import morgan from "morgan";
+import routes from "./routes/index";
 
 // Middleware
-const app = express()
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
-app.use(cors())
-app.use(morgan('dev'))
-app.use(cookieParser())
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
+app.use(morgan("dev"));
+app.use(cookieParser());
 
 // Routes
-app.use('/api', routes.authRouter)
+app.use("/api", routes.authRouter);
+app.use("/api", routes.userRouter);
 
 // Database
-import './config/database'
+import "./config/database";
 
 // server listening
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log('Server is running on port', PORT)
-})
+  console.log("Server is running on port", PORT);
+});
