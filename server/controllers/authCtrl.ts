6f2931f3c5d6915c48b76247c0254120cd0cbd70
dev-to2k <trungnek175@gmail.com>
2021-10-06 15:50:57 +0700

    #Feature: login SMS, verify SMS

--- server/controllers/authCtrl.ts
+++ server/controllers/authCtrl.ts
@@ -5,7 +5,7 @@ import {generateActiveToken, generateAccessToken, generateRefreshToken} from "..
 import sendMail from '../config/sendMail'
 import {validateEmail, validPhone} from '../middleware/valid'
 import jwt from 'jsonwebtoken'
-import {sendSms} from "../config/sendSMS";
+import {sendSms, smsOTP, smsVerify} from "../config/sendSMS";
 import {IDecodedToken, IGgPayload, IUser, IUserParams} from "../config/interface";
 import {OAuth2Client} from "google-auth-library";
 
@@ -171,6 +171,43 @@ const authCtrl = {
         }
         await registerUser(user, res)
       }
+    } catch (err: any) {
+      return res.status(500).json({msg: err.message})
+    }
+  },
+  loginSMS: async (req: Request, res: Response) => {
+    try {
+      const {phone} = req.body
+      const data = await smsOTP(phone, 'sms')
+      res.json(data)
+    } catch (err: any) {
+      return res.status(500).json({msg: err.message})
+    }
+  },
+  smsVerify: async (req: Request, res: Response) => {
+    try {
+      const {phone, code} = req.body
+
+      const data = await smsVerify(phone, code)
+      if (!data?.valid) return res.status(400).json({msg: "Invalid Authentication."})
+
+      const password = phone + 'your phone secret password'
+      const passwordHash = await bcrypt.hash(password, 12)
+
+      const user = await Users.findOne({account: phone})
+
+      if (user) {
+        await loginUser(user, password, res)
+      } else {
+        const user = {
+          name: phone,
+          account: phone,
+          password: passwordHash,
+          type: 'login'
+        }
+        await registerUser(user, res)
+      }
+
     } catch (err: any) {
       return res.status(500).json({msg: err.message})
     }
