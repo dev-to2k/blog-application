f82125a00acbe7a0b967bd3afe43a996204a08a9
dev-to2k <trungnek175@gmail.com>
2021-10-07 19:32:54 +0700

    dispatch update and delete action to store

--- client/src/redux/actions/categoryAction.ts
+++ client/src/redux/actions/categoryAction.ts
@@ -2,10 +2,13 @@ import { Dispatch } from "redux";
 import { ALERT, IAlertType } from "../types/alertTye";
 import {
   CREATE_CATEGORY,
+  DELETE_CATEGORY,
   GET_CATEGORIES,
   ICategoryType,
+  UPDATE_CATEGORY,
 } from "../types/categoryType";
-import { getAPI, postAPI } from "../../utils/FetchData";
+import { deleteAPI, getAPI, patchAPI, postAPI } from "../../utils/FetchData";
+import { ICategory } from "../../utils/TypeScript";
 
 export const createCategory =
   (name: string, token: string) =>
@@ -39,6 +42,28 @@ export const getCategories =
       });
 
       dispatch({ type: ALERT, payload: { loading: false } });
+    } catch (err: any) {
+      dispatch({ type: ALERT, payload: { errors: err.response.data.msg } });
+    }
+  };
+
+export const updateCategory =
+  (data: ICategory, token: string) =>
+  async (dispatch: Dispatch<IAlertType | ICategoryType>) => {
+    try {
+      dispatch({ type: UPDATE_CATEGORY, payload: data });
+      await patchAPI(`category/${data._id}`, { name: data.name }, token);
+    } catch (err: any) {
+      dispatch({ type: ALERT, payload: { errors: err.response.data.msg } });
+    }
+  };
+
+export const deleteCategory =
+  (id: string, token: string) =>
+  async (dispatch: Dispatch<IAlertType | ICategoryType>) => {
+    try {
+      dispatch({ type: DELETE_CATEGORY, payload: id });
+      await deleteAPI(`category/${id}`, token);
     } catch (err: any) {
       dispatch({ type: ALERT, payload: { errors: err.response.data.msg } });
     }
