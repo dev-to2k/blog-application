1321a34d8f91c8b998fa375cca1770a27d0d7e9d
dev-to2k <trungnek175@gmail.com>
2021-10-07 23:07:27 +0700

    edit toast

--- client/src/components/alert/Toast.tsx
+++ client/src/components/alert/Toast.tsx
@@ -24,7 +24,7 @@ const Toast = (props: IProps) => {
   };
 
   return (
-    <div className="w-1/3 fixed z-10 right-2 top-2">
+    <div className="w-1/3 fixed z-10 right-0 top-4">
       <div className="flex w-full relative max-w-sm mx-auto overflow-hidden bg-white rounded-lg shadow-md dark:bg-gray-800 ">
         <div className={`flex items-center justify-center w-12 ${bgColor}`}>
           {icon}
@@ -34,10 +34,20 @@ const Toast = (props: IProps) => {
             <span className={`font-semibold ${textColor} dark:text-green-400`}>
               {title}
             </span>
-            <p className="text-sm text-gray-600 dark:text-gray-200">{body}</p>
+            <div className="w-full mb-3"></div>
+            {typeof body === "string" ? (
+              body
+            ) : (
+              <ul className="text-sm list-disc pl-3 text-gray-600 dark:text-gray-200">
+                {body.map((text, index) => (
+                  <li key={index}>{text}</li>
+                ))}
+              </ul>
+            )}
+            {/*<p className="text-sm text-gray-600 dark:text-gray-200">{body}</p>*/}
           </div>
         </div>
-        <button className={`absolute right-1 top-1`} onClick={handleClose}>
+        <button className={`absolute right-2 top-2`} onClick={handleClose}>
           <XIcon className={`w-4`} />
         </button>
       </div>
