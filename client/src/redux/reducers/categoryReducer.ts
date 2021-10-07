04bfec6a4d6e8a9076149d4d29bfd3049501be6f
dev-to2k <trungnek175@gmail.com>
2021-10-07 19:33:26 +0700

    take type update and delete category

--- client/src/redux/reducers/categoryReducer.ts
+++ client/src/redux/reducers/categoryReducer.ts
@@ -12,6 +12,16 @@ const categoryReducer = (
     case types.GET_CATEGORIES:
       return action.payload;
 
+    case types.UPDATE_CATEGORY:
+      return state.map((item) =>
+        item._id === action.payload._id
+          ? { ...item, name: action.payload.name }
+          : item
+      );
+
+    case types.DELETE_CATEGORY:
+      return state.filter((item) => item._id !== action.payload);
+
     default:
       return state;
   }
