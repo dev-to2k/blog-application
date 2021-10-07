b34620513c0a2f03a66ad92d0bdc92050575ef33
dev-to2k <trungnek175@gmail.com>
2021-10-07 23:05:34 +0700

    #Router: create blog

---
+++ server/routes/blogRouter.ts
@@ -0,0 +1,9 @@
+import express from "express";
+import blogCtrl from "../controllers/blogCtrl";
+import auth from "../middleware/auth";
+
+const router = express.Router();
+
+router.post("/blog", auth, blogCtrl.createBlog);
+
+export default router;
