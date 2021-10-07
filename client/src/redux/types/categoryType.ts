51a845d52d87f8ee74755a9ce35456b6ca990377
dev-to2k <trungnek175@gmail.com>
2021-10-07 19:33:55 +0700

    create interface update and delete category

--- client/src/redux/types/categoryType.ts
+++ client/src/redux/types/categoryType.ts
@@ -2,6 +2,8 @@ import { ICategory } from "../../utils/TypeScript";
 
 export const CREATE_CATEGORY = "CREATE_CATEGORY";
 export const GET_CATEGORIES = "GET_CATEGORIES";
+export const UPDATE_CATEGORY = "UPDATE_CATEGORY";
+export const DELETE_CATEGORY = "DELETE_CATEGORY";
 
 export interface ICreateCategory {
   type: typeof CREATE_CATEGORY;
@@ -13,4 +15,18 @@ export interface IGetCategories {
   payload: ICategory[];
 }
 
-export type ICategoryType = ICreateCategory | IGetCategories;
+export interface IUpdateCategory {
+  type: typeof UPDATE_CATEGORY;
+  payload: ICategory;
+}
+
+export interface IDeleteCategory {
+  type: typeof DELETE_CATEGORY;
+  payload: string;
+}
+
+export type ICategoryType =
+  | ICreateCategory
+  | IGetCategories
+  | IUpdateCategory
+  | IDeleteCategory;
