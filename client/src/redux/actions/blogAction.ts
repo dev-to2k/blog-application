d58a12eccab7b54ed23c6e83070f5d2dbe47e9e8
dev-to2k <trungnek175@gmail.com>
2021-10-07 23:04:45 +0700

    #Action: create blog

---
+++ client/src/redux/actions/blogAction.ts
@@ -0,0 +1,28 @@
+import { IBlog } from "../../utils/TypeScript";
+import { Dispatch } from "redux";
+import { ALERT, IAlertType } from "../types/alertTye";
+import { imageUpload } from "../../utils/ImageUpload";
+import { postAPI } from "../../utils/FetchData";
+
+export const createBlog =
+  (blog: IBlog, token: string) => async (dispatch: Dispatch<IAlertType>) => {
+    let url;
+    try {
+      dispatch({ type: ALERT, payload: { loading: true } });
+
+      if (typeof blog.thumbnail !== "string") {
+        const photo = await imageUpload(blog.thumbnail);
+        url = photo.url;
+      } else {
+        url = blog.thumbnail;
+      }
+
+      const newBlog = { ...blog, thumbnail: url };
+      const res = await postAPI("blog", newBlog, token);
+      console.log(res);
+
+      dispatch({ type: ALERT, payload: { loading: false } });
+    } catch (err: any) {
+      dispatch({ type: ALERT, payload: { errors: err.response.data.msg } });
+    }
+  };
