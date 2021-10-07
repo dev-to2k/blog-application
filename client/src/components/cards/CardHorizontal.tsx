13bc34197d0dc9183ec19f1bd91ba378fbc519de
dev-to2k <trungnek175@gmail.com>
2021-10-07 21:27:03 +0700

    create card preview

---
+++ client/src/components/cards/CardHorizontal.tsx
@@ -0,0 +1,71 @@
+import React from "react";
+import { IBlog, RootStore } from "../../utils/TypeScript";
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+
+interface IProps {
+  blog: IBlog;
+}
+
+const CardHorizontal: React.FC<IProps> = ({ blog }) => {
+  const { authReducer } = useSelector((state: RootStore) => state);
+  return (
+    <div className="max-w-2xl mx-auto overflow-hidden bg-white rounded-lg shadow-md">
+      {blog.thumbnail && (
+        <>
+          {typeof blog.thumbnail === "string" ? (
+            <Link to={`/blog/${blog._id}`}>
+              <img
+                src={blog.thumbnail}
+                className="object-cover w-full h-64"
+                alt="thumbnail"
+              />
+            </Link>
+          ) : (
+            <img
+              src={URL.createObjectURL(blog.thumbnail)}
+              className="object-cover w-full h-64"
+              alt="thumbnail"
+            />
+          )}
+        </>
+      )}
+      <div className="p-6">
+        <div>
+          {blog.category.length === 0 ? (
+            ""
+          ) : (
+            <span className="text-xs px-2 uppercase font-medium bg-indigo-500 text-white rounded py-0.5">
+              {blog.category}
+            </span>
+          )}
+          <h5 className="block mt-2 text-2xl font-semibold text-gray-800 dark:text-white hover:text-gray-600">
+            {blog.title}
+          </h5>
+          <p className="mt-2 text-sm text-gray-600 dark:text-gray-400">
+            {blog.description}
+          </p>
+        </div>
+        <div className="mt-4">
+          <div className="flex items-center">
+            <div className="flex items-center">
+              <img
+                className="object-cover h-10 rounded-full"
+                src={authReducer.user?.avatar}
+                alt="Avatar"
+              />
+              <span className="mx-2 font-semibold text-gray-700 dark:text-gray-200">
+                {authReducer.user?.name}
+              </span>
+            </div>
+            <span className="mx-1 text-xs text-gray-600 dark:text-gray-300">
+              {new Date(blog.createdAt).toLocaleString()}
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CardHorizontal;
