f8e18b376a5f2f6da24730d625c05d7c0398a852
dev-to2k <trungnek175@gmail.com>
2021-10-07 23:06:55 +0700

    combine blog router

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -1,11 +1,13 @@
 import authRouter from "./authRouter";
 import userRouter from "./userRouter";
 import categoryRouter from "./categoryRouter";
+import blogRouter from "./blogRouter";
 
 const routes = {
   authRouter,
   userRouter,
   categoryRouter,
+  blogRouter,
 };
 
 export default routes;
