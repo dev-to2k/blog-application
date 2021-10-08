8de7f9c99c150b31aa8bd0562a6030951037f686
dev-to2k <trungnek175@gmail.com>
2021-10-08 13:17:36 +0700

    combine home blogs reducer

--- client/src/redux/reducers/index.ts
+++ client/src/redux/reducers/index.ts
@@ -2,9 +2,11 @@ import { combineReducers } from "redux";
 import authReducer from "./authReducer";
 import alertReducer from "./alertReducer";
 import categories from "./categoryReducer";
+import homeBlogs from "./homeBlogReducer";
 
 export default combineReducers({
   authReducer,
   alertReducer,
   categories,
+  homeBlogs,
 });
