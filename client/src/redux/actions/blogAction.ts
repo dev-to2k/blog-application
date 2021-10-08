1bf142947cbc11bca44d817bfc536a2af0feecaf
dev-to2k <trungnek175@gmail.com>
2021-10-08 13:13:46 +0700

    get home blogs

--- client/src/redux/actions/blogAction.ts
+++ client/src/redux/actions/blogAction.ts
@@ -2,7 +2,8 @@ import { IBlog } from "../../utils/TypeScript";
 import { Dispatch } from "redux";
 import { ALERT, IAlertType } from "../types/alertTye";
 import { imageUpload } from "../../utils/ImageUpload";
-import { postAPI } from "../../utils/FetchData";
+import { getAPI, postAPI } from "../../utils/FetchData";
+import { GET_HOME_BLOGS, IGetHomeBlogsType } from "../types/blogType";
 
 export const createBlog =
   (blog: IBlog, token: string) => async (dispatch: Dispatch<IAlertType>) => {
@@ -20,6 +21,24 @@ export const createBlog =
       const newBlog = { ...blog, thumbnail: url };
       const res = await postAPI("blog", newBlog, token);
       console.log(res);
+
+      dispatch({ type: ALERT, payload: { loading: false } });
+    } catch (err: any) {
+      dispatch({ type: ALERT, payload: { errors: err.response.data.msg } });
+    }
+  };
+
+export const getHomeBlogs =
+  () => async (dispatch: Dispatch<IAlertType | IGetHomeBlogsType>) => {
+    try {
+      dispatch({ type: ALERT, payload: { loading: true } });
+
+      const res = await getAPI("home/blogs");
+
+      dispatch({
+        type: GET_HOME_BLOGS,
+        payload: res.data,
+      });
 
       dispatch({ type: ALERT, payload: { loading: false } });
     } catch (err: any) {
