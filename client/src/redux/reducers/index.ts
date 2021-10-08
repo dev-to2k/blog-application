940f12e2597e6f6109eee0768e45d9b5409382a8
dev-to2k <trungnek175@gmail.com>
2021-10-08 17:11:21 +0700

    combine blogs category

--- client/src/redux/reducers/index.ts
+++ client/src/redux/reducers/index.ts
@@ -3,10 +3,12 @@ import authReducer from "./authReducer";
 import alertReducer from "./alertReducer";
 import categories from "./categoryReducer";
 import homeBlogs from "./homeBlogReducer";
+import blogsCategory from "./blogsCategoryReducer";
 
 export default combineReducers({
   authReducer,
   alertReducer,
   categories,
   homeBlogs,
+  blogsCategory,
 });
