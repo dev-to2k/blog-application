40b7a45a7bf0f067a8a35ac5d6d5d7016c21c147
dev-to2k <trungnek175@gmail.com>
2021-10-08 17:09:56 +0700

    #Router: get blogs by category

--- server/routes/blogRouter.ts
+++ server/routes/blogRouter.ts
@@ -8,4 +8,6 @@ router.post("/blog", auth, blogCtrl.createBlog);
 
 router.get("/home/blogs", blogCtrl.getHomeBlogs);
 
+router.get("/blogs/:category_id", blogCtrl.getBlogsByCategory);
+
 export default router;
