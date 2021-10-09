70d7873e3351a902497372518523cb661e1b8347
dev-to2k <trungnek175@gmail.com>
2021-10-09 14:55:25 +0700

    avatar will appear when have access token

--- client/src/components/cards/CardPreview.tsx
+++ client/src/components/cards/CardPreview.tsx
@@ -10,7 +10,7 @@ interface IProps {
 const CardPreview: React.FC<IProps> = ({ blog }) => {
   const { authReducer } = useSelector((state: RootStore) => state);
   return (
-    <div className="max-w-2xl mx-auto overflow-hidden bg-white rounded-lg shadow-md">
+    <div className="max-w-xl mx-auto overflow-hidden bg-white rounded-lg shadow-md">
       {blog.thumbnail && (
         <>
           {typeof blog.thumbnail === "string" ? (
@@ -33,7 +33,7 @@ const CardPreview: React.FC<IProps> = ({ blog }) => {
       <div className="p-6">
         <div>
           <h5 className="block mt-2 text-2xl font-semibold text-gray-800 dark:text-white hover:text-gray-600">
-            {blog.title}
+            <Link to={`/blog/${blog._id}`}>{blog.title}</Link>
           </h5>
           <p className="mt-2 text-sm text-gray-600 dark:text-gray-400">
             {blog.description}
@@ -42,11 +42,13 @@ const CardPreview: React.FC<IProps> = ({ blog }) => {
         <div className="mt-4">
           <div className="flex items-center">
             <div className="flex items-center">
-              <img
-                className="object-cover h-10 rounded-full"
-                src={authReducer.user?.avatar}
-                alt="Avatar"
-              />
+              {authReducer?.access_token && (
+                <img
+                  className="object-cover h-10 rounded-full"
+                  src={authReducer.user?.avatar}
+                  alt="Avatar"
+                />
+              )}
               <span className="mx-2 font-semibold text-gray-700 dark:text-gray-200">
                 {authReducer.user?.name}
               </span>
