dc3a5c69c6884734479375eea3a22757a7c6e22c
dev-to2k <trungnek175@gmail.com>
2021-10-09 14:17:44 +0700

    #Type: get other info

---
+++ client/src/redux/types/profileType.ts
@@ -0,0 +1,8 @@
+import { IUser } from "../../utils/TypeScript";
+
+export const GET_OTHER_INFO = "GET_OTHER_INFO";
+
+export interface IGetOtherInfoType {
+  type: typeof GET_OTHER_INFO;
+  payload: IUser;
+}
