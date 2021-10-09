ad31dd4073e29d239fa5d9efcbe68a30a2af951b
dev-to2k <trungnek175@gmail.com>
2021-10-09 16:20:28 +0700

    re-format "User" to "user"

--- server/models/userModel.ts
+++ server/models/userModel.ts
@@ -1,37 +1,41 @@
-import mongoose from 'mongoose'
-import {IUser} from "../config/interface";
+import mongoose from "mongoose";
+import { IUser } from "../config/interface";
 
-const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, "Please add your name"],
-    trim: true,
-    maxLength: [20, "Your name is up to 20 chars long."]
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please add your name"],
+      trim: true,
+      maxLength: [20, "Your name is up to 20 chars long."],
+    },
+    account: {
+      type: String,
+      required: [true, "Please add your email or phone"],
+      trim: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Please add your password"],
+    },
+    avatar: {
+      type: String,
+      default:
+        "https://cdn.dribbble.com/users/77598/screenshots/15303455/media/8bf1868b09614e5f1fe2e3225cad4a00.png",
+    },
+    role: {
+      type: String,
+      default: "user", // admin
+    },
+    type: {
+      type: String,
+      default: "register", // fast
+    },
   },
-  account: {
-    type: String,
-    required: [true, "Please add your email or phone"],
-    trim: true,
-    unique: true
-  },
-  password: {
-    type: String,
-    required: [true, "Please add your password"]
-  },
-  avatar: {
-    type: String,
-    default: 'https://cdn.dribbble.com/users/77598/screenshots/15303455/media/8bf1868b09614e5f1fe2e3225cad4a00.png'
-  },
-  role: {
-    type: String,
-    default: 'user' // admin
-  },
-  type: {
-    type: String,
-    default: 'register' // fast
+  {
+    timestamps: true,
   }
-}, {
-  timestamps: true
-})
+);
 
-export default mongoose.model<IUser>('User', userSchema)
+export default mongoose.model<IUser>("user", userSchema);
