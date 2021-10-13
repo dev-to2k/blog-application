7d4b7d2360d6aba67e0c4a697a5e1c3928894a5c
dev-to2k <trungnek175@gmail.com>
2021-10-13 20:00:13 +0700

    #Router: create comment

---
+++ server/routes/commentRouter.ts
@@ -0,0 +1,9 @@
+import express from 'express'
+import commentCtrl from '../controllers/commentCtrl'
+import auth from '../middleware/auth'
+
+const router = express.Router()
+
+router.post('/comment', auth, commentCtrl.createComment)
+
+export default router
