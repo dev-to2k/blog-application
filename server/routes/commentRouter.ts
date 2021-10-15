3668c81cd29c50814b2fbb3cea9a8c30d04c3c38
dev-to2k <trungnek175@gmail.com>
2021-10-15 12:49:46 +0700

    #Router: get comments

--- server/routes/commentRouter.ts
+++ server/routes/commentRouter.ts
@@ -1,9 +1,11 @@
-import express from 'express'
-import commentCtrl from '../controllers/commentCtrl'
-import auth from '../middleware/auth'
+import express from "express";
+import commentCtrl from "../controllers/commentCtrl";
+import auth from "../middleware/auth";
 
-const router = express.Router()
+const router = express.Router();
 
-router.post('/comment', auth, commentCtrl.createComment)
+router.post("/comment", auth, commentCtrl.createComment);
 
-export default router
+router.get("/comments/blog/:id", commentCtrl.getComments);
+
+export default router;
