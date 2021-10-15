e47973af86288462ae39377ea6738f365b34e64f
dev-to2k <trungnek175@gmail.com>
2021-10-15 12:48:36 +0700

    #Action: get comments

--- client/src/redux/actions/commentAction.ts
+++ client/src/redux/actions/commentAction.ts
@@ -1,10 +1,15 @@
 import { Dispatch } from 'redux'
 
 import { ALERT, IAlertType } from '../types/alertTye'
-import { CREATE_COMMENT, ICreateCommentType } from '../types/commentType'
+import {
+  CREATE_COMMENT,
+  GET_COMMENTS,
+  ICreateCommentType,
+  IGetCommentsType,
+} from '../types/commentType'
 
 import { IComment } from '../../utils/TypeScript'
-import { postAPI } from '../../utils/FetchData'
+import { getAPI, postAPI } from '../../utils/FetchData'
 
 export const createComment =
   (data: IComment, token: string) =>
@@ -17,6 +22,25 @@ export const createComment =
       dispatch({
         type: CREATE_COMMENT,
         payload: { ...res.data, user: data.user },
+      })
+    } catch (err: any) {
+      dispatch({ type: ALERT, payload: { errors: err.response.data.msg } })
+    }
+  }
+
+export const getComments =
+  (id: string) => async (dispatch: Dispatch<IAlertType | IGetCommentsType>) => {
+    try {
+      let limit = 8
+
+      const res = await getAPI(`comments/blog/${id}?limit=${limit}`)
+
+      dispatch({
+        type: GET_COMMENTS,
+        payload: {
+          data: res.data.comments,
+          total: res.data.total,
+        },
       })
     } catch (err: any) {
       dispatch({ type: ALERT, payload: { errors: err.response.data.msg } })
