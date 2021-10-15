56156fb5e5cd256b270303f920a32f68641e0dd6
dev-to2k <trungnek175@gmail.com>
2021-10-15 13:26:50 +0700

    #Feature: create reply comment

--- client/src/components/comments/Comments.tsx
+++ client/src/components/comments/Comments.tsx
@@ -1,14 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { IComment } from '../../utils/TypeScript'
 
 import AvatarComment from './AvatarComment'
 import CommentList from './CommentList'
+import AvatarReply from './AvatarReply'
 
 interface IProps {
   comment: IComment
 }
 
 const Comments: React.FC<IProps> = ({ comment }) => {
+  const [showReply, setShowReply] = useState<IComment[]>([])
   return (
     <div
       className="flex mb-6"
@@ -19,7 +21,29 @@ const Comments: React.FC<IProps> = ({ comment }) => {
     >
       <AvatarComment user={comment.user} />
 
-      <CommentList comment={comment} />
+      <CommentList
+        comment={comment}
+        showReply={showReply}
+        setShowReply={setShowReply}
+      >
+        {showReply.map((comment, index) => (
+          <div
+            key={index}
+            style={{
+              opacity: comment._id ? 1 : 0.5,
+              pointerEvents: comment._id ? 'initial' : 'none',
+            }}
+          >
+            <AvatarReply user={comment.user} reply_user={comment.reply_user} />
+
+            <CommentList
+              comment={comment}
+              showReply={showReply}
+              setShowReply={setShowReply}
+            />
+          </div>
+        ))}
+      </CommentList>
     </div>
   )
 }
