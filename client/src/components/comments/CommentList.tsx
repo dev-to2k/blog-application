48759505243250985c17ef2306bd95bc7cd420c4
dev-to2k <trungnek175@gmail.com>
2021-10-15 13:26:33 +0700

    #Feature: create reply comment

--- client/src/components/comments/CommentList.tsx
+++ client/src/components/comments/CommentList.tsx
@@ -1,12 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
-import { IComment } from '../../utils/TypeScript'
+import { useDispatch, useSelector } from 'react-redux'
+
+import { IComment, RootStore } from '../../utils/TypeScript'
+
+import Input from './Input'
 
 interface IProps {
   comment: IComment
+  showReply: IComment[]
+  setShowReply: (showReply: IComment[]) => void
 }
 
-const CommentList: React.FC<IProps> = ({ comment }) => {
+const CommentList: React.FC<IProps> = ({
+  children,
+  comment,
+  showReply,
+  setShowReply,
+}) => {
+  const [onReply, setOnReply] = useState(false)
+  const { authReducer } = useSelector((state: RootStore) => state)
+  const dispatch = useDispatch()
+
+  const handleReply = (body: string) => {
+    if (!authReducer.user || !authReducer.access_token) return
+
+    const data = {
+      user: authReducer.user,
+      blog_id: comment.blog_id,
+      blog_user_id: comment.blog_user_id,
+      content: body,
+      reply_user: comment.user,
+      comment_root: comment._id,
+      createdAt: new Date().toISOString(),
+    }
+    console.log(data)
+    setShowReply([...showReply, data])
+    setOnReply(false)
+  }
   return (
     <div>
       <Link
@@ -23,8 +54,16 @@ const CommentList: React.FC<IProps> = ({ comment }) => {
             __html: comment.content,
           }}
         />
-        <small className="cursor-pointer">Reply to</small>
+        <small
+          className="cursor-pointer text-indigo-500 font-semibold"
+          onClick={() => setOnReply(!onReply)}
+        >
+          {onReply ? 'Cancel' : 'Reply to'}
+        </small>
       </div>
+      {onReply && <Input callback={handleReply} />}
+
+      {children}
     </div>
   )
 }
