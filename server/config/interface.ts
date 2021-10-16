ffeab3612c24455c2b67c9c0ba43268aea963525
dev-to2k <trungnek175@gmail.com>
2021-10-16 10:56:36 +0700

    #Interface: add field comment_root for IComment

--- server/config/interface.ts
+++ server/config/interface.ts
@@ -1,54 +1,55 @@
-import { Document } from 'mongoose'
-import { Request } from 'express'
+import { Document } from "mongoose";
+import { Request } from "express";
 
 export interface IUser extends Document {
-  name: string
-  account: string
-  password: string
-  avatar: string
-  role: string
-  type: string
-  _doc: object
+  name: string;
+  account: string;
+  password: string;
+  avatar: string;
+  role: string;
+  type: string;
+  _doc: object;
 }
 
 export interface INewUser {
-  name: string
-  password: string
-  account: string
+  name: string;
+  account: string;
+  password: string;
 }
 
 export interface IDecodedToken {
-  id?: string
-  newUser?: INewUser
-  iat: number
-  exp: number
+  id?: string;
+  newUser?: INewUser;
+  iat: number;
+  exp: number;
 }
 
 export interface IGgPayload {
-  email: string
-  email_verified: boolean
-  name: string
-  picture: string
+  email: string;
+  email_verified: boolean;
+  name: string;
+  picture: string;
 }
 
 export interface IUserParams {
-  name: string
-  account: string
-  password: string
-  avatar?: string
-  type: string
+  name: string;
+  account: string;
+  password: string;
+  avatar?: string;
+  type: string;
 }
 
 export interface IReqAuth extends Request {
-  user?: IUser
+  user?: IUser;
 }
 
 export interface IComment extends Document {
-  user: string
-  blog_id: string
-  blog_user_id: string
-  content: string
-  replyCM: string[]
-  reply_user: string
-  _doc: object
+  user: string;
+  blog_id: string;
+  blog_user_id: string;
+  content: string;
+  replyCM: string[];
+  reply_user: string;
+  comment_root: string;
+  _doc: object;
 }
