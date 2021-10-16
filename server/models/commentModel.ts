da8f68cf036e7750cf469b521be4cec7b8dac845
dev-to2k <trungnek175@gmail.com>
2021-10-16 10:54:31 +0700

    #Model: add field comment_root

--- server/models/commentModel.ts
+++ server/models/commentModel.ts
@@ -1,18 +1,19 @@
-import mongoose from 'mongoose'
-import { IComment } from '../config/interface'
+import mongoose from "mongoose";
+import { IComment } from "../config/interface";
 
 const commentSchema = new mongoose.Schema(
   {
-    user: { type: mongoose.Types.ObjectId, ref: 'user' },
+    user: { type: mongoose.Types.ObjectId, ref: "user" },
     blog_id: mongoose.Types.ObjectId,
     blog_user_id: mongoose.Types.ObjectId,
     content: { type: String, required: true },
-    replyCM: [{ type: mongoose.Types.ObjectId, ref: 'comment' }],
-    reply_user: { type: mongoose.Types.ObjectId, ref: 'user' },
+    replyCM: [{ type: mongoose.Types.ObjectId, ref: "comment" }],
+    reply_user: { type: mongoose.Types.ObjectId, ref: "user" },
+    comment_root: { type: mongoose.Types.ObjectId, ref: "comment" },
   },
   {
     timestamps: true,
   }
-)
+);
 
-export default mongoose.model<IComment>('comment', commentSchema)
+export default mongoose.model<IComment>("comment", commentSchema);
