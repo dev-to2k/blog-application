238a188ada83fa913e3a5879a5c388943edc7618
dev-to2k <trungnek175@gmail.com>
2021-10-16 10:52:53 +0700

    add name for avatar reply

--- client/src/components/comments/AvatarReply.tsx
+++ client/src/components/comments/AvatarReply.tsx
@@ -13,13 +13,24 @@ const AvatarReply: React.FC<IProps> = ({ user, reply_user }) => {
       <img
         src={user.avatar}
         alt="avatar"
-        className="hidden object-cover w-10 h-10 mr-4 rounded-full sm:block"
+        className="hidden object-cover w-10 h-10 mr-3 rounded-full sm:block"
       />
 
-      <div className="ms-1">
+      <div className="ms-1 flex flex-col gap-3">
+        <small className="font-semibold">
+          <Link to={`/profile/${user._id}`} style={{ textDecoration: 'none' }}>
+            {user.name}
+          </Link>
+        </small>
+
         <small className="reply-text">
           Reply to{' '}
-          <Link to={`/profile/${reply_user?._id}`}>{reply_user?.name}</Link>
+          <Link
+            to={`/profile/${reply_user?._id}`}
+            className="font-semibold text-indigo-500"
+          >
+            {reply_user?.name}
+          </Link>
         </small>
       </div>
     </div>
