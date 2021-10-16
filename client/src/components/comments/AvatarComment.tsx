0497c781934093eaa196ddb1737e2159e6814d23
dev-to2k <trungnek175@gmail.com>
2021-10-16 10:52:23 +0700

    add name for avatar comment

--- client/src/components/comments/AvatarComment.tsx
+++ client/src/components/comments/AvatarComment.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { IUser } from '../../utils/TypeScript'
+import { Link } from 'react-router-dom'
 
 interface IProps {
   user: IUser
@@ -7,12 +8,15 @@ interface IProps {
 
 const AvatarComment: React.FC<IProps> = ({ user }) => {
   return (
-    <div>
+    <div className="mr-3">
       <img
-        className="hidden object-cover w-10 h-10 mx-4 rounded-full sm:block"
+        className="hidden object-cover w-10 h-10 mx-auto rounded-full sm:block"
         src={user.avatar}
         alt="avatar"
       />
+      <small className="font-semibold block mt-2">
+        <Link to={`/profile/${user._id}`}>{user.name}</Link>
+      </small>
     </div>
   )
 }
